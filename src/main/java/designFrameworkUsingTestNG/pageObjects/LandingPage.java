5f4c3d657bfee33ace66f61b4e6faffc7fb18e4d
Swapnil Chaudhari <swap95nil@gmail.com>
2026-04-01 20:04:15 +0530

    Now we just added locators for login page using traditional driver.findelement(By) way We will convert them to pagefactory

--- src/main/java/designFrameworkUsingTestNG/pageObjects/LandingPage.java
+++ src/main/java/designFrameworkUsingTestNG/pageObjects/LandingPage.java
@@ -3,11 +3,16 @@ package designFrameworkUsingTestNG.pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LandingPage {
 	WebDriver driver;
+
 	public LandingPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	WebElement userName = driver.findElement(By.id("userEmail"));
+
+	WebElement userNamefield = driver.findElement(By.id("userEmail"));
+	WebElement passwordfield = driver.findElement(By.id("userPassword"));
+	WebElement loginBtn = driver.findElement(By.id("login"));
 }
