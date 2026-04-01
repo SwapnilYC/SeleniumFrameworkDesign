4865ddec80400e35e9e1bcfe38a71b1794a0bffb
Swapnil Chaudhari <swap95nil@gmail.com>
2026-04-01 19:00:33 +0530

    complete end to end standalone test code is ready

--- src/test/java/DesignPrinciplesOfSeleniumFrameWork/UsingTestNG_TestingFramework/StandAloneTest.java
+++ src/test/java/DesignPrinciplesOfSeleniumFrameWork/UsingTestNG_TestingFramework/StandAloneTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -18,7 +19,8 @@ public class StandAloneTest {
 		WebDriver driver = new ChromeDriver();
 //		driver.manage().window().setSize(new Dimension(1366, 768));
 
-		driver.manage().window().setSize(new Dimension(1920, 1080)); // for CI/CD this dimention is recommended
+//		driver.manage().window().setSize(new Dimension(1920, 1080)); // for CI/CD this dimention is recommended
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		// 1. Go to landing page
@@ -50,16 +52,24 @@ public class StandAloneTest {
 
 		// 9. Wait for all elements to be loaded in cart section
 		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart")));
-		
-		// 10. Save all those items in a list & verify that our added product is present
-		isProductPresentIncart( driver);
-		
-		// 11. Go to checkOut page
-		driver.findElement(By.xpath("//div[contains(@class,'subtotal')]//button")).click();
-		
-		
 
-//		driver.quit();
+		// 10. Save all those items in a list & verify that our added product is present
+		isProductPresentIncart(driver, productName);
+
+		// 11. Go to checkOut page
+		goToCheckoutPage(driver, wait1);
+
+		// 12. select country
+		String countryInitials = "ind";
+		String countryName = "India";
+		selectCountry(driver, wait1, countryInitials, countryName);
+
+		// 13. go to Order summary page
+		goToOrderConfirmationPage(driver, wait1);
+		
+		//14. Quit the browser
+		driver.quit();
+
 	}
 
 // Go to URL
@@ -90,15 +100,11 @@ public class StandAloneTest {
 
 // Wait for toast message to appear & loader to get disappear
 	public static void toasterAndLoaderWait(WebDriverWait wait1, WebDriver driver) {
-		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("toast-container"))); // wait to load toaster
-																								// success message
-		wait1.until(ExpectedConditions.invisibilityOfElementLocated(By.id("toast-container")));
-//		WebElement loaderOverlay = driver.findElement(By.className("ng-animating"));
-		wait1.until(ExpectedConditions.invisibilityOfElementLocated(By.className("ng-animating")));
-//		wait1.until(ExpectedConditions.invisibilityOf(loaderOverlay)); // wait to disappear
-
+		// wait to load toaster success message
+		WebElement loaderOverlay = driver.findElement(By.cssSelector(".ng-animating"));
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("toast-container"))); 															
+		wait1.until(ExpectedConditions.invisibilityOf(loaderOverlay));
 		// These waits run sequentially: First wait completes Then second wait starts
-
 	}
 
 // Click on cart button & go to cart page
@@ -110,9 +116,45 @@ public class StandAloneTest {
 	}
 
 // Save all those items in a list & verify that our added product is present
-	public static void isProductPresentIncart(WebDriver driver) {
+	public static void isProductPresentIncart(WebDriver driver, String productName) {
 		List<WebElement> cartProducts = driver.findElements(By.xpath("//div[@class='cart']//li"));
 		Assert.assertTrue(cartProducts.stream()
 				.anyMatch(e -> e.findElement(By.tagName("h3")).getText().equalsIgnoreCase(productName)));
 	}
+
+// Go to checkout page
+	public static void goToCheckoutPage(WebDriver driver, WebDriverWait wait1) {
+		driver.findElement(By.xpath("//div[contains(@class,'subtotal')]//button")).click(); // click on checkout btn
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input [@class='input txt'])[1]")));
+		driver.findElement(By.xpath("(//input [@class='input txt'])[1]")).sendKeys("123"); // csv
+	}
+
+// Select country
+	public static void selectCountry(WebDriver driver, WebDriverWait wait1, String countryInitials,
+			String countryName) {
+		WebElement countrydropdown = driver.findElement(By.xpath("//input[@placeholder='Select Country']"));
+//		countrydropdown.sendKeys(countryInitials);
+		Actions a = new Actions(driver);
+		a.sendKeys(driver.findElement(By.xpath("//input[@placeholder='Select Country']")), countryInitials).build().perform();
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section [class*=list-group]")));
+		List<WebElement> countryList = driver.findElements(By.cssSelector("section [class*=list-group] button"));
+		WebElement selectedCountry = countryList.stream()
+				.filter(country -> country.findElement(By.cssSelector("span")).getText().equalsIgnoreCase(countryName))
+				.findFirst().orElse(null);
+		if (selectedCountry != null) {
+			selectedCountry.click();
+		} else {
+			System.out.println("Country not found. Please try again later");
+			driver.quit();
+		}
+	}
+
+// Go to order summary page
+	public static void goToOrderConfirmationPage(WebDriver driver, WebDriverWait wait1) {
+		driver.findElement(By.xpath("// div[@class='actions']//a")).click();
+		wait1.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.tagName("h1")));
+		String actualConfirmMessage = driver.findElement(By.tagName("h1")).getText();
+		Assert.assertTrue(actualConfirmMessage.equalsIgnoreCase("Thankyou for the order."));
+	}
+
 }
